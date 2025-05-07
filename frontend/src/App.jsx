625c9b6ae2575f92f34ce7b62342642415ee3dca
LowYingJie <lowyingjie02@gmail.com>
2025-05-07 17:00:54 +0800

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,1 +1,20 @@
 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import DoctorList from './pages/DoctorList';
+import BookAppointment from './pages/BookAppointment';
+import Appointments from './pages/Appointments';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/doctors" element={<DoctorList />} />
+        <Route path="/book/:doctorId" element={<BookAppointment />} />
+        <Route path="/appointments" element={<Appointments />} />
+        {/* Add login/register/dashboard routes here */}
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
