b15e0248cb3e18a990a46fba676d8c579e6a3e61
LowYingJie <lowyingjie02@gmail.com>
2025-05-07 17:02:59 +0800

    Create Message.js

---
+++ backend/models/Message.js
@@ -0,0 +1,12 @@
+// backend/models/Message.js
+
+const mongoose = require('mongoose');
+
+const MessageSchema = new mongoose.Schema({
+  senderId: String,
+  receiverId: String,
+  message: String,
+  timestamp: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Message', MessageSchema);
